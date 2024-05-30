c40d13642dda804e63ed64a5dee767567f14ba75
hoangvh238 <hoangvh238.dev@gmail.com>
2024-05-30 12:59:19 +0700

    Init source base

---
+++ src/test/java/com/backend/camp_scholar/CampScholarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.camp_scholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampScholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
